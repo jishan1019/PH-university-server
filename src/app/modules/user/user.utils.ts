9c1d3fe0bef91d6c895c3582753f57d37bdfddc7
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-30 12:35:47 +0600

    student semester year and code issue solve

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,7 +1,7 @@
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { UserModel } from './user.model';
 
-const findLastStudent = async () => {
+const findLastStudentId = async () => {
   const lastStudent = await UserModel.findOne(
     {
       role: 'student',
@@ -11,11 +11,26 @@ const findLastStudent = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudent()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastSemesterCode = lastStudentId?.substring(4, 6); //01
+  const lastStudentYear = lastStudentId?.substring(0, 4); //2030
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
 
