5b94d2f099d8425035b9a44f891355e09743f1ed
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-30 19:43:41 +0600

    tow or many data write isolated environment create using session

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { AcademicSemesterModel } from '../academicSemester/academicSemester.model';
@@ -6,27 +7,57 @@ import { StudentModel } from '../student/student.model';
 import { TUser } from './user.interface';
 import { UserModel } from './user.model';
 import { generateStudentId } from './user.utils';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
 
 const createStudentIntoDb = async (password: string, payload: TStudent) => {
-  const userUser: Partial<TUser> = {};
+  const userData: Partial<TUser> = {};
 
   //find academic semester info
   const admissionSemester = await AcademicSemesterModel.findById(
     payload.admissionSemester,
   );
 
-  userUser.password = password || (config.default_pass as string);
-  userUser.role = 'student';
-  userUser.id = await generateStudentId(admissionSemester as TAcademicSemester);
+  userData.password = password || (config.default_pass as string);
+  userData.role = 'student';
 
-  const newUser = await UserModel.create(userUser);
+  const session = await mongoose.startSession();
 
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id; //embed id
-    payload.user = newUser._id; //reference id
+  try {
+    //start session
+    session.startTransaction();
 
-    const newStudent = await StudentModel.create(payload);
+    userData.id = await generateStudentId(
+      admissionSemester as TAcademicSemester,
+    );
+
+    //session (transaction - 1)
+    const newUser = await UserModel.create([userData], { session });
+
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create a new user');
+    }
+
+    payload.id = newUser[0].id; //embed id
+    payload.user = newUser[0]._id; //reference id
+
+    //session (transaction - 2)
+    const newStudent = await StudentModel.create([payload], { session });
+
+    if (!newUser.length) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'Failed to create a new student',
+      );
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
     return newStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
 
