55a4bf6f1f624555821e4e5b51d1d211026dee21
jishan <imranislamjishan80@gmail.com>
2024-06-05 01:59:14 +0600

    handel unhandledRejection and uncaughtException

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
+import { Server } from 'http';
+
+let server: Server;
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Server Running on port ${config.port}`);
     });
   } catch (error) {
@@ -15,3 +18,20 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+//unhandledRejection for asynchronous code
+//unhandledRejection for synchronous code
+
+process.on('unhandledRejection', () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  process.exit(1);
+});
