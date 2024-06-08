db8dc57c2a535f0dd119fb9750343983e621681c
jishan <imranislamjishan80@gmail.com>
2024-06-08 18:18:39 +0600

    course validation schema create

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { CourseController } from './course.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { courseValidationSchema } from './course.validation';
 const router = express.Router();
 
 router.get('/', CourseController.getAllCourses);
 router.get('/:id', CourseController.getSingleCourses);
-router.post('/', CourseController.createCourse);
+router.post(
+  '/',
+  validateRequest(courseValidationSchema),
+  CourseController.createCourse,
+);
 router.delete('/:id', CourseController.deleteCourse);
 
 export const CourseRoutes = router;
---
+++ src/app/modules/Course/course.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const preRequisiteCoursesValidationSchema = z.object({
+  course: z.string(),
+  isDeleted: z.boolean().optional(),
+});
+
+const courseValidationSchema = z.object({
+  body: z.object({
+    title: z.string({
+      invalid_type_error: 'title must be string',
+    }),
+    prefix: z.string({
+      invalid_type_error: 'prefix must be string',
+    }),
+    code: z.number({
+      invalid_type_error: 'prefix must be number',
+    }),
+    credits: z.number({
+      invalid_type_error: 'credits must be number',
+    }),
+    preRequisiteCourses: z.array(preRequisiteCoursesValidationSchema),
+  }),
+});
+
+export { courseValidationSchema };
