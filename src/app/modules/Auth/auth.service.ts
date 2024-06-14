d299a8ca1730728d77028204673865cf18a34429
jishan <imranislamjishan80@gmail.com>
2024-06-14 11:00:56 +0600

    login user pass verify

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,6 +1,37 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { UserModel } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import argon2 from 'argon2';
 
-const loginUserFromDb = async (payload: TLoginUser) => {};
+const loginUserFromDb = async (payload: TLoginUser) => {
+  const isUserExist = await UserModel.findOne({ id: payload?.id });
+  if (!isUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  const isDeleted = isUserExist?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This User is deleted');
+  }
+
+  const userStatus = isUserExist?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.NOT_FOUND, 'This User is Blocked');
+  }
+
+  const isPasswordMatch = await argon2.verify(
+    isUserExist?.password,
+    payload?.password,
+  );
+
+  if (!isPasswordMatch) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Password is wrong. Please try again',
+    );
+  }
+};
 
 export const AuthServices = {
   loginUserFromDb,
