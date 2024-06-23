3dca0a379b029394fdda805bd461ae36a70aa3a0
jishan <imranislamjishan80@gmail.com>
2024-06-23 20:16:50 +0600

    enrol course model interface and vlidation schema create

---
+++ src/app/modules/EnrollCourse/enrollCourse.model.ts
@@ -0,0 +1,111 @@
+import mongoose, { Schema } from 'mongoose';
+import {
+  TEnrolledCourse,
+  TEnrolledCourseMarks,
+} from './enrollCourse.interface';
+import { Grade } from './enrollCourse.constatnt';
+
+const courseMarksSchema = new Schema<TEnrolledCourseMarks>(
+  {
+    classTest1: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    midTerm: {
+      type: Number,
+      min: 0,
+      max: 30,
+      default: 0,
+    },
+    classTest2: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    finalTerm: {
+      type: Number,
+      min: 0,
+      max: 50,
+      default: 0,
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const enrolledCourseSchema = new Schema<TEnrolledCourse>({
+  semesterRegistration: {
+    type: Schema.Types.ObjectId,
+    ref: 'semesterRegistration',
+    required: true,
+  },
+  academicSemester: {
+    type: Schema.Types.ObjectId,
+    ref: 'academicSemester',
+    required: true,
+  },
+  academicFaculty: {
+    type: Schema.Types.ObjectId,
+    ref: 'academicFaculty',
+    required: true,
+  },
+  academicDepartment: {
+    type: Schema.Types.ObjectId,
+    ref: 'academicDepartment',
+    required: true,
+  },
+  offeredCourse: {
+    type: Schema.Types.ObjectId,
+    ref: 'offeredCourse',
+    required: true,
+  },
+  course: {
+    type: Schema.Types.ObjectId,
+    ref: 'courseFaculty',
+    required: true,
+  },
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: 'Student',
+    required: true,
+  },
+  faculty: {
+    type: Schema.Types.ObjectId,
+    ref: 'faculty',
+    required: true,
+  },
+  isEnrolled: {
+    type: Boolean,
+    default: false,
+  },
+  courseMarks: {
+    type: courseMarksSchema,
+    default: {},
+  },
+  grade: {
+    type: String,
+    enum: Grade,
+    default: 'NA',
+  },
+  gradePoints: {
+    type: Number,
+    min: 0,
+    max: 4,
+    default: 0,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const EnrolledCourse = mongoose.model<TEnrolledCourse>(
+  'EnrolledCourse',
+  enrolledCourseSchema,
+);
+
+export default EnrolledCourse;
